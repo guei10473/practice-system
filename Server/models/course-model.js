41a91bfe8a072d6df7844ee829bb5a9878cb1dcf
guei10473 <guei10473@gmail.com>
2023-05-06 04:14:55 +0800

    5.6 commit

---
+++ Server/models/course-model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  id: { type: String },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
