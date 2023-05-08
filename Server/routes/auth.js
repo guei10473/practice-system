996dc6dd2f5f393a391d3d2aa8d75ab17b583aeb
guei10473 <guei10473@gmail.com>
2023-05-09 01:35:09 +0800

    5.9 commit

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const registerValidation = require("../validation").registerValidation;
 const loginValidation = require("../validation").loginValidation;
 const User = require("../models").userModel;
+const jwt = require("jsonwebtoken");
 
 router.use((req, res, next) => {
   console.log("A request is coming in to auth.js");
@@ -45,6 +46,33 @@ router.post("/register", async (req, res) => {
   } catch (err) {
     res.status(400).send("User not saved");
     console.log(err);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  // check the validation of data
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = await User.findOne({ email: req.body.email });
+
+  try {
+    if (!user) {
+      res.status(401).send("User not found.");
+    } else {
+      user.comparePassword(req.body.password, function (err, isMatch) {
+        if (err) return res.status(400).send(err);
+        if (isMatch) {
+          const tokenObject = { _id: user._id, email: user.email };
+          const token = jwt.sign(tokenObject, process.env.PASSPORT_SECRET);
+          res.send({ success: true, token: "JWT" + token, user });
+        } else {
+          res.status(401).send("Wrong password.");
+        }
+      });
+    }
+  } catch (error) {
+    res.status(400).send(err);
   }
 });
 
