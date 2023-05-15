8e9b272cc15289378cffa25e8da1685009223a48
guei10473 <guei10473@gmail.com>
2023-05-15 09:21:58 +0800

    新增course功能/驗證

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 const authRoute = require("./routes").auth;
+const courseRoute = require("./routes").course;
+const passport = require("passport");
+require("./config/passport")(passport);
 
 // connect to DB
 
@@ -24,6 +27,7 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/user", authRoute);
+app.use("/api", passport.authenticate("jwt", { session: false }), courseRoute);
 
 //
 
